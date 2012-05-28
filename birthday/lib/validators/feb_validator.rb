05e086d84ac9bef250c15e402a90b7bcd77b291d
Daniel Ruiz <ynnadrules@gmail.com>
2012-05-28 16:50:49 -0400

    changed feb validator back to a Validator instead of EachValidator. fixed the error messages for day and month in the en locale yml file. added some thoughts about sanitizers in the issues.txt'

--- birthday/lib/validators/feb_validator.rb
+++ birthday/lib/validators/feb_validator.rb
@@ -1,15 +1,24 @@
-class FebValidator < ActiveModel::EachValidator
-  def validate_each( record, attr_name, value )
+class FebValidator < ActiveModel::Validator
+  def validate( record )
     if record.month == 2
       unless record.day >= 1 and record.day < 30 #and !record.day and f(record.day) > 3 then
-        record.errors.add( attr_name, :day, options.merge( value: value ) )
+        record.errors[:day] << "day must be between 1 and 29"
+        #record.errors.add( attr_name, :day, options.merge( value: value ) )
       end 
     end
   end
 end
 
-module ActiveModel::Validations::HelperMethods
-  def validates_february( *attr_names )
-    validates_with FebValidator, _merge_attributes( attr_names )
-  end
-end
+# class FebValidator < ActiveModel::Validator
+#   def validate( record )
+#     puts "validating #{record.inspect}"
+#     record.errors[:day] = "error in day"
+#   end
+# end
+
+# module ActiveModel::Validations::HelperMethods
+#   def validates_february( *attr_names )
+#     validates_with FebValidator, _merge_attributes( attr_names )
+#   end
+# end
+
