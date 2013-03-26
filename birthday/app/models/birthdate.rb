46ee93da25dfd8c2bada08ee3ae51b6be9c4bbfe
Mike <mcueno2@uic.edu>
2013-03-26 13:23:47 -0500

    Trying to switch from data aquisition phase back to implementation

--- birthday/app/models/birthdate.rb
+++ birthday/app/models/birthdate.rb
@@ -1,54 +1,17 @@
-#With the various ways of adding validators on a model we can do something like this:
-
-# Model Schema
-# year  integer
-# month integer
-# day   integer
 
 class Birthdate < ActiveRecord::Base
-  include Cenforce
-  # validates :year, :month, :day, presence: true, numericality: true
 
-  # validates :month, :day, feb: true
+   validates :year, :month, :day, presence: true, numericality: true
 
-  # validate :my_custom_validator
+#   validates :month, :day, feb: true
 
-  #jvalidate :year, :month, :day
+   validate :validate_feb
 
-  cenforce :valid_date do
-    remote      :false
-    attributes  :month, :day
-    error       :day, "Invalid days for this month"
-    validator   :check_feb do 
-      :valid unless month == 2 && ( day > 29 || day < 1 )
-    end
-  end
+#   validate :year, :month, :day
 
-  # def my_custom_validator
-  #   errors.add( "day", "day is greater than 29!!!" ) if day > 29 and month == 2
-  #   #my rails dependent validation
-  #   client_validation :feb_validator do
-  #     req :day, :month
+   def validate_feb
+     errors.add( "day", "day is greater than 29!!!" ) if day > 29 and month == 2
+   end
 
-  #     #default error message will be stored in locale files but can be overwritten here
-  #     fail "day is greater than 29!!!"
-
-  #     client_validate do
-  #       if month == 2
-  #         day > 29 or day <= 0
-  #       end
-  #     end
-  #   end
-
-  # end
-  # def sanitize_me (record)
-  #   if record.month == 2 then
-  #     unless record.day >= 1 and record.day < 30
-  #       #record.errors[:day] << "days in february must be between 1 and 29"
-  #       record.day = 1
-  #     end
-  #   end
-  #   return true
-  # end
 end
 
