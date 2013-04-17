d717d6ef146f191f909c0b132a10b159d5da69c0
Mike <mcueno2@uic.edu>
2013-04-16 19:17:02 -0500

    Added arithmetic and fixed a few bugs

--- examiner/lib/data_acquisition/rsbc_helper.rb
+++ examiner/lib/data_acquisition/rsbc_helper.rb
@@ -1,14 +1,14 @@
 module RsbcHelper
 
   require 'set'
-  Supported_funcs = [:<,:>,:==,:>=,:<=]
+  Supported_funcs = [:<,:>,:==,:>=,:<=, :-, :+, :*, :/]
   Database_calls = [:find, :find_by_sql, :find_all, :find_first, :find_last, :where, :all, :delete, :delete_all, :destroy, :destroy_all,
                     :exists?, :get_primary_key, :last, :inspect, :reset_column_information, :reset_columns, :reset_pimary_key, :reset_counters,
                     :reset_subclasses, :rest_table_name, :set_primary_key, :set_readonly_option!, :set_sequence_name, :set_table_name, 
                     :set_inheritance_column, :table_exists?, :update, :update_all]
 
   Temp_filename = "/tmp/constraints"
-  Plato_path = "~/rsbc/externals/clicl/bin/clicl"
+  Plato_path = "~/Research/rsbc/externals/clicl/bin/clicl"
   Outdir = "#{Rails.root}/app/assets/javascripts/"    
   @@vars = Set.new()
 
@@ -24,10 +24,10 @@ module RsbcHelper
   # mimic_server_side_validation(Birthdate) # Logs errors to /log/rsbc.log # puts validation js in /app/assets/javascripts/client
   def mimic_server_side_validation(model)
     # convert validator code to logic (KIF) and generate JS validation code
+    puts "now working on #{model}"
     @model = model
     validations = get_validations(model)
-    validations = remove_unwanted(validations)
-    #kifstring = "(and" 
+    validations = remove_unwanted(validations)  # We only want custom defined validations 
     kifstring = ""
     validations.each do |validator| 
       if validator.class == Proc then mimic_on_proc(validator) else
@@ -36,21 +36,30 @@ module RsbcHelper
       if validator.methods.include? :source_location then log_location(validator.source_location) end
       begin 
         puts @owner.to_s + " " + @method_name.to_s
-        kifstring = ruby2plato(validator)[0]
+        kifstring = kifstring + ruby2plato(validator)[0]
+        puts kifstring
           if check_plato(kifstring) then log_success() else log_failure(kifstring) end
       rescue Exception => exc      # May want to my more explicit and include cases for differant exceptions to make logs more useful
         log_unknown(exc)
       end
       end
     end
-    #kifstring += ")"
-    validfile = Outdir + model.name + "_validation.js"
-    #invoke_plato(kifstring,validfile)
 
-    # create sanitization code from model's typing info
-    sanifile = Outdir + model.name + "_sanitization.js"
-   # create_sanitization(model,@@vars,sanifile)  # TODO Not sure if this is ok, (Using the class variable @@vars, don't think it should matter)
-    return [validfile,sanifile]
+    #### TESTING 
+#    kifstring =   " (=> (not (== (+ ?adults ?children ) ?travelers)) false)" 
+    #### TESTING 
+
+    # Produce the JS 
+    validfile = Outdir + model.name + "_validation.js"    # This is plato's output
+    helperfile = Outdir + model.name + "_rsbchelper.js"   # Contains the type checking information 
+    if check_plato(kifstring) and !kifstring.empty?
+      invoke_plato(kifstring,validfile)
+      create_helpers(model,@@vars,helperfile) 
+    end
+
+    puts @@vars.inspect
+    @@vars.clear
+    return [validfile,helperfile]
   end
 
   # Run the translator on a Proc, this is usually the case when a validator is inside a gem 
@@ -87,11 +96,11 @@ module RsbcHelper
   def get_validations(model)
     validators = model._validate_callbacks.map {|callback| callback.raw_filter}     # Gives the Validators or the symbols (name of model defined validation methods) 
     model_method_names = validators.collect {|x| if x.class == Symbol then x end }  # example would be   :validate_month
-    model_method_names.delete_if{|x| x == nil }					    
+    model_method_names.delete_if{|x| x == nil }             
     gem_validators = validators.collect {|x| if x.instance_values["block"] then x.instance_values["block"]  end}  # Returns a Proc with the validation code
     gem_validators.delete_if {|x| x == nil }
     validators.delete_if {|x| x.instance_values["block"] }
-    validators.delete_if {|x| x.class == Symbol } 			                      	    # separating the model validations and the validator validations
+    validators.delete_if {|x| x.class == Symbol }                                   # separating the model validations and the validator validations
     methods = get_methods(validators)
     model_method_names.each do |def_name| methods << model.instance_method(def_name) end     #    Building array of UnboundMethods -----/ 
     methods.concat gem_validators
@@ -112,7 +121,7 @@ module RsbcHelper
         methods << v.class.instance_method(:validate)  # default / this happens in ruby 1.8.7 becuase the 'source_location' method is not included until 1.9.3
       end
     end
-    methods 
+    methods  # return 
   end
 
   # used to only grab Validators and Explicitly defined validations in the model
@@ -122,36 +131,103 @@ module RsbcHelper
     validators.delete_if {|x| if x.public_methods.include? :owner then x.owner != @model and !(x.owner < ActiveModel::Validator) end } 
   end
 
-  def create_sanitization(model,vars,filename)
-  	h = Hash.new  # hash keyed on string of field name
-  	model.columns.map {|x| h[x.name]=x.type}
-  	f = File.new(filename, "w")
-  	f.puts "function sanitize(field) {"
-  	f.puts '  var val = $("input[name=\"" + field + "\"]").val();'
-  	f.puts "  switch(field) {"
-  	# note that vars is a set of symbols
-  	vars.each {|v| f.print "    case '#{v}': return "
-  				   f.print(rails_type_to_js_typecast(h[v.to_s]))
-  				   f.puts "(val);" }
-  	f.puts "    otherwise: return val;"
-  	f.puts "  }"
-  	f.puts "}"
-  	f.close
+  def create_helpers(model,vars,filename)
+    h = Hash.new  # hash keyed on string of field name
+    m_name = model.name.downcase
+    model.columns.map {|x| h[x.name]=x.type}
+    f = File.new(filename, "w")
+    f.puts "function sanitize(field) {"
+    f.puts "  var val = $(\"input##{m_name}_\" + field ).val();"
+    f.puts "  if(!val){val = $(\"select##{m_name}_\" + field ).val();}"  # If not an input, try a select tag 
+    f.puts "  switch(field) {"
+    # note that vars is a set of symbols
+    vars.each {|v| f.print "    case '#{v}': return "
+             f.print(rails_type_to_js_typecast(h[v.to_s]))
+             f.puts "(val);" }
+    f.puts "    otherwise: return val;"
+    f.puts "  }"
+    f.puts "}"
+
+    # Fields in the model that need validating 
+    f.print "var fields = ["
+    vars.each {|v| f.print " '#{v}',"}
+    f.puts "];"
+
+
+    # Plato needs these functions defined 
+    f.puts "function hascellvalue( field ) {"
+    f.puts "  var val = $( 'input##{m_name}_' + field ).val();"
+    f.puts "  if(!val){val = $(\"select##{m_name}_\" + field).val(); }" 
+    f.puts "  return val.length > 0; "
+    f.puts "}"
+    f.puts "function cellvalue( field ) {"
+    f.puts "  return sanitize(field);"
+    f.puts "}"
+
+    f.puts "function validate( field, x ) {"
+    f.puts "  var i = window['neg_' + field + \"_b\"].apply( this, [x_func, cellvalue( field )] );"
+    f.puts "  var haserror = i instanceof expr;                                                   "
+    f.puts "  if (haserror) { console.log( \"[\" + field + \"] has an error: \", i ); }           "
+    f.puts "                                                                                      "
+    f.puts "  var $field = $( 'input##{m_name}_' + field ).parent();                              "
+    f.puts "                                                                                      "
+    f.puts "  if( haserror ) {                                                                    "
+    f.puts "                                                                                      "
+    f.puts "    if( !$field.hasClass( 'field_with_error' ) ){                                     "
+    f.puts "      $field.addClass( 'field_with_error' );                                          "
+    f.puts "      $('<span/>').html( \"Error with this field\" ).appendTo( $field );              "
+    f.puts "    }                                                                                 "
+    f.puts "  } else {                                                                            "
+    f.puts "    if( $field.hasClass( 'field_with_error' ) ) {                                     "
+    f.puts "      $field.removeClass( 'field_with_error' );                                       "
+    f.puts "      $field.find( 'span' ).html( \"\" );                                             "
+    f.puts "    }                                                                                 "
+    f.puts "  }                                                                                   "
+    f.puts "  return i;                                                                           "
+    f.puts "}                                                                                     "
+
+    
+    f.puts "function bind() {"
+    f.puts "  $.each( fields, function( i, e ) {"
+    f.puts "    console.log( 'binding ' + e );"
+    f.puts "    console.log( 'input##{m_name}_' + e );"
+    f.puts "    $( 'input##{m_name}_' + e ).change( e, function( evt ) {"
+    f.puts "      dependent[e].mapc( validate );"
+    f.puts "    });"
+    f.puts "  });"
+    f.puts "  $( '#new_#{m_name}' ).submit( fields, function( evt ) {"
+    f.puts "    console.log( 'submission attempt' );"
+    f.puts "    var valid = true;"
+    f.puts "    $.each( evt.data, function( i, e ) {"
+    f.puts "      var result = validate( e ) instanceof expr;"
+    f.puts "      if( result ) {"
+    f.puts "        valid = false;"
+    f.puts "      }"
+    f.puts "    });"
+    f.puts "    console.log( valid );"
+    f.puts "    return valid;"
+    f.puts "  })"
+    f.puts "}"
+    f.puts "$(document).ready(function() {"
+    f.puts "  bind();"
+    f.puts "});"
+    f.close
   end
   
   def rails_type_to_js_typecast(type)
-  	case type
-    	when :string, :text then "String"
-    	when :boolean then "Boolean"
-    	when :integer then "parseInt"
-    	when :decimal, :float then "parseFloat"
-    	else "String"
-  	end
+    case type
+      when :string, :text then "String"
+      when :boolean then "Boolean"
+      when :integer then "parseInt"
+      when :decimal, :float then "parseFloat"
+      else "String"
+    end
   end
-	
+  
   def invoke_plato(constraints,filename)
     # constraints must be in KIF (i.e. a string)
     # convert ruby code to plato logic and write to file
+    puts "#{constraints}"
     f = File.new(Temp_filename,"w")
     f.puts(constraints)
     f.close
@@ -170,13 +246,13 @@ module RsbcHelper
     f.puts(constraints)
     f.close
     # Check if reader gets same as string
-    print "S-expression check:"
+    #print "S-expression check:"
     cmd = Plato_path + ' --eval "(progn (handler-case (read-file \\"' + Temp_filename + '\\") (condition () (quit 1))) (quit 0))"'
     system(cmd)
     noerr = $?.success?
-    if noerr then puts "Passed" else puts "Failed" end
+    #if noerr then puts "Passed" else puts "Failed" end
     if noerr then
-    puts "Plato check (if errors, transformed constraints printed, then errors printed):"
+    #puts "Plato check (if errors, transformed constraints printed, then errors printed):"
     cmd = Plato_path + ' --eval "(if (pl-fhl-to-js-check (maksand (read-file \\"' + Temp_filename + '\\"))) (quit 1) (quit 0))"'
     system(cmd)
     noerr &= $?.success?
@@ -190,8 +266,8 @@ module RsbcHelper
     @@plato_output = StringIO.new
     
     # Run on blocks: to restore, remove next 14 lines, uncomment ruby2kif
-    ast = s.to_ast
-    if ast[0] == :defn 
+    ast = s.to_ast              # Grab ast 
+    if ast[0] == :defn          
       vars = ast[2][1]
         if ast[3][0] == :scope and ast[3][1][0] == :block 
           block = ast[3][1]
@@ -206,8 +282,8 @@ module RsbcHelper
     else puts "ERROR: cannot run on blocks"; log_unknown(ast)
     end
            
-  	if s.class == Proc then ruby2kif(s.to_ast,nil) end
-  	return @@plato_output.string, @@vars
+    if s.class == Proc then ruby2kif(s.to_ast,nil) end
+    return @@plato_output.string, @@vars
   end
 
   # Takes an sexpression and the name of the toplevel variable that holds the object being validated
@@ -216,20 +292,20 @@ module RsbcHelper
   def ruby2kif (sexp, var)
     return unless sexp 
     case sexp[0]
-  	when :defn then ruby2kif(sexp[3],sexp[2][1])  #(:defn name (:args v1 v2...) stmt)
-  	when :scope then ruby2kif(sexp[1],var)
-  	when :block then  parseBlock(sexp, var) #There could be more than one satement in the block 
-  	when :call then rubyterm2kif(sexp, var)
-    when :return then ruby2kif(sexp[1],var)
+    when :defn then ruby2kif(sexp[3],sexp[2][1])  #(:defn name (:args v1 v2...) stmt)
+    when :scope then ruby2kif(sexp[1],var)
+    when :block then  parseBlock(sexp, var) #There could be more than one satement in the block 
+    when :call then rubyterm2kif(sexp, var)
+    when :return then pk(" "); ruby2kif(sexp[1],var)
     when :true then pk("true")
     when :false then pk("false")
     when :if  # then sexp  # (:if cond then else)
-		if sexp[2]
-		  op; pk("=> "); ruby2kif(sexp[1],var); sp; ruby2kif(sexp[2],var); cl;
-		end
-		if sexp[3] # else
+    if sexp[2]
+      op; pk("=> "); ruby2kif(sexp[1],var); sp; ruby2kif(sexp[2],var); cl;
+    end
+    if sexp[3] # else
       op;pk("=> " ); op;pk("not "); ruby2kif(sexp[1],var);cl; sp; ruby2kif(sexp[3],var);cl
-		end
+    end
  
     when :and #(:and x y)
       op; pk("and "); ruby2kif(sexp[1],var); sp; ruby2kif(sexp[2],var); cl
@@ -266,11 +342,11 @@ module RsbcHelper
   end
 
   def rubyterm2kif(sexp, var)
-	return unless sexp
-	if ruby_error_sink(sexp) then
-		pk("false")
- 	else
-	  sexp = ruby_simplifier(sexp,var)		# Translate : to ? for plato 
+  return unless sexp
+  if ruby_error_sink(sexp) then       # Something was added to the errors array in this statment (Record is invalid) 
+    pk("false")
+  else
+    sexp = ruby_simplifier(sexp,var)    # Translate : to ? for plato 
     case sexp[0]
     when :call then rubycall2kif(sexp,var)
       else rubyobj2kif(sexp,var) end
@@ -279,15 +355,15 @@ module RsbcHelper
 
   
   def rubycall2kif(sexp, var)
-	return if !sexp or sexp[0] != :call   # (:call thing op arglist)
-	unless catch_nil(sexp, var) or catch_database(sexp)
-	  op; rubyfunc2kif(sexp,var);  # (op
-	  if sexp[1] then sp; rubyterm2kif(sexp[1],var) end  # thing added to start of arglist
-	  args = sexp[3]
-	  args = args[1..args.length-1]
-	  args.each { |a| sp; rubyterm2kif(a,var) }  # (rest of) arglist
-	  cl;    # closing paren
-	end
+  return if !sexp or sexp[0] != :call   # (:call thing op arglist)
+  unless catch_nil(sexp, var) or catch_database(sexp)
+    op; rubyfunc2kif(sexp,var);  # (op
+    if sexp[1] then sp; rubyterm2kif(sexp[1],var) end  # thing added to start of arglist
+    args = sexp[3]
+    args = args[1..args.length-1]
+    args.each { |a| sp; rubyterm2kif(a,var) }  # (rest of) arglist
+    cl;    # closing paren
+  end
   end
 
   # output ruby function to KIF function  
@@ -298,14 +374,18 @@ module RsbcHelper
     when :<= then pk("lte");
     when :>= then pk("gte");
     when :== then pk("==");
+    when :+ then pk("+");
+    when :- then pk("-"); 
+    when :* then pk("*");
+    when :/ then pk("/"); 
     when :nil? then log_func("nil?")
     else 
       log_func(sexp[2])
-		end
+    end
   end
 
   # Catches the case that the call's first argument is nil in which case two things can be happening
-  # 1. The call is referencing an instance variable OR 
+  # 1. The call is referencing an instance variable (Through ruby's getter setter attr_accessible method) OR 
   # 2. the call is making an external method call
   # Sexp should look like: (nil, attr)
   #
@@ -317,11 +397,8 @@ module RsbcHelper
   def catch_nil(sexp, var)
     if sexp[1].nil?
       if @model.columns.map{|x| x.name.to_sym}.include? sexp[2] then   # columns = instance variables / this is the first case
-        # 1.a if(!sexp[2].contains("_id"))
-          pk("true")
-        # 1.b else (AJAX)
-          # For now just ignore all cases. THIS IS WRONG!!!
-          # query database to see if sexp[2] exists and has contents? 
+          pk("?#{sexp[2]}")
+          @@vars.add(sexp[2].to_s)
         return true
       else
         begin 
@@ -359,9 +436,9 @@ module RsbcHelper
 
   # simplify form-field references from x.field to "?field"
   def ruby_simplifier (sexp,var)
- 	#print "\n\nruby_simplifier("
-  	#print sexp
-  	#puts ")\n\n"
+  #print "\n\nruby_simplifier("
+    #print sexp
+    #puts ")\n\n"
 
     if sexp and var and sexp[0] === :call and sexp[1] and sexp[1][0] === :lvar \
        and sexp[1][1] === var then
@@ -374,7 +451,7 @@ module RsbcHelper
       @@vars.add sexp[3][1][1]
       [:ivar, "?" + sexp[3][1][1]]
     else
-    	sexp
+      sexp
     end
   end
     
